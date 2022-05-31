35ba6a436fd4276d0e497005b801764f6c6f3677
Filip <47699264+SeezoCode@users.noreply.github.com>
2022-05-31 08:40:45 +0200

    Basic functionality and navigation

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("articles/", views.articles, name="articles"),
+    path("articles/<int:article_id>/", views.article, name="articleView"),
+    path("user_detail/<str:nickname>/", views.user, name="userView")
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,55 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Article, User
+from .models import Comment
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('polls/index.html')
+    latest_articles = Article.objects.order_by('-timestamp')[:3]
+    top_authors = User.objects.order_by('nickname')[:6]
+    l = []
+    for latest_article in latest_articles:
+        latest_article.content = " ".join(latest_article.content.split(" ")[:12]) + "..."
+        l.append(latest_article)
+    context = {
+        "latest_articles": l,
+        "users": top_authors
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def articles(request):
+    latest_articles = Article.objects.order_by('-timestamp')[:500]
+    template = loader.get_template('polls/articles_view.html')
+    context = {
+        "latest_articles": latest_articles,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def article(request, article_id):
+    articleReq = Article.objects.get(id=article_id)
+    commentReq = Comment.objects.filter(article=article_id).order_by("timestamp")
+
+    template = loader.get_template("polls/article_view.html")
+    context = {
+        "article": articleReq,
+        "comments": commentReq
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def user(request, nickname):
+    user = User.objects.get(nickname=nickname)
+    commentReq = Comment.objects.filter(author=user.id)
+    articleReq = Article.objects.filter(author=user.id)
+
+    template = loader.get_template('polls/user_view.html')
+    context = {
+        "user": user,
+        "comments": commentReq,
+        "articles": articleReq
+    }
+    return HttpResponse(template.render(context, request))
